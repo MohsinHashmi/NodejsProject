fa7ecf231b91437588f74c7c6bf337fd9c5d0222
MohsinHashmi <maahhashmi47@gmail.com>
2023-01-14 23:16:33 +0500

    after some change in model

--- mongose_model.js
+++ mongose_model.js
@@ -1,10 +1,12 @@
 const mongoos=require('mongoose');
 const Productscheme=new mongoos.Schema({
-    username:String,
-    password:String,
-    application:Number,
-    address:String,
-    title:String,
-    remarks:String
+    Email:String,
+    Password:String,
+    // username:String,
+    // password:String,
+    // application:Number,
+    // address:String,
+    // title:String,
+    // remarks:String
 })
 module.exports=mongoos.model('test_col',Productscheme);
